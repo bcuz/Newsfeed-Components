3c2badbf0472a6fcc4bc6face1706ca0aff0ca7c
adam <bcuz@users.noreply.github.com>
2019-02-05 22:52:07 -0500

    stretch close menu on outside click, doesnt work perfect

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,8 +1,24 @@
 
-const toggleMenu = () => {
+const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu refence. 
+  // toggleClick func will fire on everything but .menuButton due to this stop.
+  event.stopPropagation()
+  
   menu.classList.toggle('menu--open')
   
+}
+
+const toggleClick = (event) => {
+  // console.log(event.target.tagName);
+
+  let dontCloseOn = ['LI', 'UL']
+  
+  if (menu.classList.contains('menu--open') && !dontCloseOn.includes(event.target.tagName)) {
+    // console.log('hi');
+
+    menu.classList.toggle('menu--open')
+    
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -13,3 +29,4 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', toggleMenu)
+document.addEventListener('click', toggleClick)
